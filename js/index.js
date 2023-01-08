b04adf6fc9a144ad3e371627d009bff5380236f9
ivanbelikov1337 <120462639+ivanbelikov1337@users.noreply.github.com>
2023-01-08 06:03:45 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// Burger Menu
+
+const hamb = document.querySelector("#hamburger");
+const popup = document.querySelector("#popup");
+const body = document.body;
+const menu = document.querySelector("#menu").cloneNode(1);
+hamb.addEventListener("click", hambHandler);
+function hambHandler(e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    hamb.classList.toggle("active");
+    body.classList.toggle("no-scroll");
+    renderPopup();
+}
+function renderPopup() {
+    popup.appendChild(menu);
+}
+
+const links = Array.from(menu.children);
+
+links.forEach((link) => {
+    link.addEventListener("click", closeOnClick);
+});
+
+function closeOnClick() {
+    popup.classList.remove("open");
+    hamb.classList.remove("active");
+    body.classList.remove("no-scroll");
+}
